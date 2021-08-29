241d97e99d06a9f6984e03675ed5b5bc0f1912f5
Sheri Tsao <yinyungsharee08@gmail.com>
2021-08-28 18:32:04 -0700

    small changes on card

--- src/components/event-lists/Upcoming.js
+++ src/components/event-lists/Upcoming.js
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -10,8 +11,7 @@ const Upcoming = () => {
   const useStyles = makeStyles((theme) => ({
     root: {
       maxWidth: 345,
-      height: 120,
-      padding: 8,
+      height: 130,
     },
     content: {
       padding: 8,
@@ -27,20 +27,22 @@ const Upcoming = () => {
   return (
     <Container maxWidth="sm" className={classes.root}>
       <Card className={classes.root}>
-        <CardContent className={classes.content}>
-          <Typography className={classes.title}>
-            Event name
-          </Typography>
-          <Typography>
-            Event host
-          </Typography>
-          <Typography>
-            Event date {`&`} time
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Attend</Button>
-        </CardActions>
+        <CardActionArea>
+          <CardContent className={classes.content}>
+            <Typography className={classes.title}>
+              Event name
+            </Typography>
+            <Typography>
+              Event host
+            </Typography>
+            <Typography>
+              Event date {`&`} time
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Attend</Button>
+          </CardActions>
+        </CardActionArea>
       </Card>
     </Container>
   );
