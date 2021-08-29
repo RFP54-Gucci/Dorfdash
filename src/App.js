7ce37e4cf527bfb67a04d6303efc1dd7dff5df7f
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-08-29 13:32:23 -0700

    added two components RiderForm and EventDetails

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import EventDetails from './components/events/eventDetails.js';
+import RiderForm from './components/events/riderForm.js';
 
 function App() {
   return (
     <div className="App">
+      {/* <EventDetails /> */}
+      <RiderForm />
     </div>
   );
 }
