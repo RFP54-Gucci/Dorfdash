74b7933fc56743813d52de35b391a3b62b6ccfce
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-30 11:16:33 -0700

    git ignoring build and package lock

--- src/components/homepage/newUser.js
+++ src/components/homepage/newUser.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
+import { Container, AppBar, Typography } from '@material-ui/react';
 
 const NewUser = () => {
-
+  <Container>
+  <AppBar position="static">
+  <Typography variant="h3" className="header header-title">
+          Dorfdash
+        </Typography>
+      </AppBar>
+  <Container className="footer">
+        <p>Designed by Team GUCCI @ 2021</p>
+      </Container>
+    </Container>
 }
 
 export default NewUser;
