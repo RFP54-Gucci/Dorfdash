5c1ad1e1695a9bf4bf6fc3054a076ab70d0f76a5
Sho Kondo <notshokondo@gmail.com>
2021-08-30 16:50:07 -0700

    changed query from req.params to req.query

--- server/models/events.js
+++ server/models/events.js
@@ -2,9 +2,7 @@ const db = require('../../db/index.js');
 
 module.exports = {
   getEvent: async (req, callback) => {
-    const { event_name } = req.headers;
-    console.log(event_name);
-
+    const { event_name } = req.query;
     const queryStr = `SELECT * FROM events WHERE event_name='${event_name}'`;
 
     try {
@@ -18,14 +16,11 @@ module.exports = {
     const { event_name, host, date, time, description, location } = req.body;
     const queryStr = `INSERT INTO events (event_name, host, date, time, description, location) VALUES ('${event_name}', '${host}', '${date}', '${time}', '${description}', '${location}')`;
 
-    console.log(queryStr);
-
     try {
       await db.query(queryStr);
       callback(null);
     } catch (err) {
       callback(err);
     }
-
   },
 }
