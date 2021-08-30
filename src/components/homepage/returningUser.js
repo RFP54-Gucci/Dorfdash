798378577cf4ed64954bc3bbf01fc66b577b64c6
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-30 16:13:20 -0700

    styling returninguser with material ui

--- src/components/homepage/returningUser.js
+++ src/components/homepage/returningUser.js
@@ -1,7 +1,48 @@
 import { useState } from 'react';
 import { Container, AppBar, Typography, TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  div: {
+    width: '100vw',
+    maxWidth: '100vw',
+    padding: '0',
+    height: '100vh'
+  },
+
+  header: {
+    backgroundColor: '#20A46B'
+  },
+  headerTitle: {
+    margin: '0',
+    padding: '2%',
+  },
+  footer: {
+    width: '100vw',
+    maxWidth: '100vw',
+    backgroundColor: '#3F3F3F',
+    padding: '2% 2% 5% 2%',
+    textAlign: 'center',
+    color: '#fff'
+  },
+  form: {
+    padding: '5%',
+    margin: '13% auto',
+    borderRadius: '25px',
+    boxShadow: '0px 5px 22px 0px rgba(0,0,0,0.65)'
+  },
+  button: {
+    backgroundColor: '#20A46B',
+    color: '#fff',
+    marginTop: '10%',
+    fontSize: '1.2rem'
+  }
+});
+
 
 const ReturningUser = () => {
+
+  const classes = useStyles();
 
   const [email, setEmail] = useState('');
 
@@ -17,21 +58,21 @@ const ReturningUser = () => {
   }
 
   return (
-    <Container>
-      <AppBar position="static">
-        <Typography variant="h3" className="header header-title">
+    <Container className={classes.div}>
+      <AppBar position="static" className={classes.header}>
+        <Typography variant="h3" className={classes.headerTitle}>
           Dorfdash
         </Typography>
       </AppBar>
-      <Container className="form outline" maxWidth="xs">
+      <Container className={classes.form} maxWidth="xs">
       <h2>Welcome Back!</h2>
       <TextField fullWidth={true} id="filled-basic" label="Email" variant="filled" required margin="normal"
         onChange={(e) => {handleEmail(e)}}/>
-      <Button className="signupBtn" variant="contained" disableElevation
+      <Button className={classes.button} variant="contained" disableElevation
         onClick={(e) => {handleSubmit(e)}}
       >Log In</Button>
     </Container>
-      <Container className="footer">
+      <Container className={classes.footer}>
         <p>Designed by Team GUCCI @ 2021</p>
       </Container>
     </Container>
