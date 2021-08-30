e144bb1fe1a555e0b160fb4224efa50c4d8c8b7f
Jinhoo Bong <jinhoobong@gmail.com>
2021-08-30 11:35:33 -0700

    setting up newuser page; ;

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import EventForm from './components/event-lists/EventForm.js';
 import Upcoming from './components/event-lists/Upcoming.js';
 
 import Homepage from './components/homepage/homePage.js';
-// import NewUser from './src/components/homepage/newUser.js';
+import NewUser from './components/homepage/newUser.js'
 
 function App() {
   return (
     <div className="App">
       {/* <EventForm/>
       <Upcoming/> */}
-      <Homepage/>
-      {/* <NewUser/> */}
+      {/* <Homepage/> */}
+      <NewUser/>
     </div>
   );
 }
