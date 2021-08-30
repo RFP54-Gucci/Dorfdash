e4d410a8d62655b16546e3f6fcc900304039e11d
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-08-29 23:18:41 -0700

    added an avatar for the rider and also a checkbox for the driver to choose the riders from the list

--- src/components/events/riderForm.js
+++ src/components/events/riderForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Container, Paper, Grid, Button, TextField} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -29,7 +29,7 @@ const useStyles = makeStyles((theme) => ({
    textAlign:'left'
  },
  textField: {
-   padding:10,
+   padding:13,
    width:300
  },
  city: {
@@ -48,18 +48,32 @@ const useStyles = makeStyles((theme) => ({
  zip: {
    width:80,
    padding:10
- }
+ },
+ gridStyle: {
+   height:400,
+  paddingTop:5,
+ backgroundColor:'#ECECEC'
+}
 }));
 
 
 const RiderForm = () => {
+
+  const[addRider, setAddRider] = useState({
+    phone: '',
+    addressLine1: '',
+    addressLine2: '',
+    city: '',
+    state: '',
+    zipcode: '',
+  })
   const classes = useStyles();
   return (
     <Container maxWidth="xs" className={classes.container}>
       <h1>Dorfdash</h1>
-    <Grid container justify="center" alignItems="center">
+    <Grid container justify="center" alignItems="center" className={classes.gridStyle}>
         <Grid item xs={12}>
-          <Paper className={classes.paper} style ={{marginRight:40}}>
+
             <p style={{fontStyle:'italic'}}>Enter your contact details*</p>
           <form className={classes.root} Validate autoComplete="off">
             <TextField id="outlined-basic" label="Contact Number *"
@@ -70,7 +84,7 @@ const RiderForm = () => {
              variant="outlined" className={classes.textField} />
              <br />
 
-             <TextField id="outlined-basic" label="Address Line 2 *"
+             <TextField id="outlined-basic" label="Address Line 2"
              variant="outlined" className={classes.textField} />
               <br />
              <TextField id="outlined-basic" label="City *"
@@ -81,19 +95,24 @@ const RiderForm = () => {
 
             <TextField id="outlined-basic" label="Zip code *"
              variant="outlined" className={classes.zip} />
-
-
           </form>
-          </Paper>
+
         </Grid>
     </Grid>
+
     <Button variant="contained"  className={classes.root}
          style={{backgroundColor: '#12824C', color: '#FFFFFF', margin: 40}}>
+  Back
+</Button>
+
+    <Button variant="contained"  className={classes.root}
+         style={{backgroundColor: '#12824C', color: '#FFFFFF', margin: 40}}
+         onClick = {() => alert(addRider.phone)}>
   Submit
 </Button>
   </Container>
   )
 //sta
 }
-
+//fe
 export default RiderForm;
