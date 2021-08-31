552b5238570f3670964bfd9abe3c97bb5ce87111
Lakshmy Mohan Erakandath <lakshmitalks@gmail.com>
2021-08-31 12:33:37 -0700

    created two sample rider information for the driver see and their distance to the driver

--- src/components/events/driverForm.js
+++ src/components/events/driverForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Container, Avatar, Grid, Paper, Typography, TextField} from '@material-ui/core';
+import {Container, Avatar, Grid, Paper,Button} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from "react-router-dom";
 import Checkbox from '@material-ui/core/Checkbox';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -17,7 +19,9 @@ const useStyles = makeStyles((theme) => ({
  paper: {
   padding: theme.spacing(2),
   textAlign: 'center',
-  color: theme.palette.text.secondary,
+  color: "black",
+  backgroundColor:'#ECECEC',
+
 },
 typography: {
   backgroundColor:'#ECECEC',
@@ -27,6 +31,9 @@ avatar: {
     width: theme.spacing(7),
     height: theme.spacing(7),
     margin:4
+},
+riderInfo: {
+  padding:5
 }
 }))
 
@@ -34,28 +41,70 @@ const DriverForm = () => {
   const classes = useStyles();
   const [checked, setChecked] = React.useState(true);
 
+  let history = useHistory();
+
+
+
   const handleChange = (event) => {
     setChecked(event.target.checked);
   };
+  const handleSubmit = () => {
+    history.push("/map");
+  }
   return (
     <Container maxWidth="xs" className={classes.container}>
       <h1>Dorfdash</h1>
-      <p style = {{fontStyle:'italic'}}>Select your Riders</p>
+      <p style = {{fontStyle:'italic'}}>Hey Helio! Select your Riders</p>
+
       <Grid container spacing={2} style={{paddingBottom:8}}>
         <Grid>
         <Avatar className={classes.avatar} />
         </Grid>
         <Grid item xs={8}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <Paper className={classes.paper} >
+            <div className={classes.riderInfo}>Taurus</div>
+            <div className={classes.riderInfo}>(123)456-789</div>
+            <div className={classes.riderInfo}>21 Pike Place</div>
+          </Paper>
         </Grid>
         <Grid item xs={0}>
           <Checkbox
         defaultChecked
         color="primary"
+        onChange={handleChange}
         inputProps={{ 'aria-label': 'secondary checkbox' }}
       />
+      <div>1.1mi</div>
+          </Grid>
+
+        <Grid container spacing={2} style={{paddingBottom:8}}>
+        <Grid>
+        <Avatar className={classes.avatar} />
+        </Grid>
+        <Grid item xs={8}>
+          <Paper className={classes.paper} >
+            <div className={classes.riderInfo}>Kair</div>
+            <div className={classes.riderInfo}>(123)223-789</div>
+            <div className={classes.riderInfo}>21 Einstein Place</div>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={0}>
+          <Checkbox
+        defaultChecked
+        color="primary"
+        onChange={handleChange}
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <div>1.5mi</div>
           </Grid>
       </Grid>
+      </Grid>
+      <Button variant="contained"  className={classes.root}
+         style={{backgroundColor: '#12824C', color: '#FFFFFF', margin: 40}}
+         onClick={handleSubmit}>
+  Finish
+</Button>
   </Container>
   )
 }
