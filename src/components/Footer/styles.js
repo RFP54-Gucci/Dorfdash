591deda8e71be35704a61e80848bcd82c2189250
Isaiah Love <isaiahlove805@yahoo.com>
2021-08-31 12:11:53 -0700

    updated with header and footer and map designs

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,15 @@
+
+  import { makeStyles } from '@material-ui/core/styles';
+  export default makeStyles(() => ({
+    Footer:{
+    marginTop: '2rem',
+    color: '#fff',
+    height: '3.8rem',
+    justifycontent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3F3F3F',
+    display: 'flex',
+    }
+  }));
+
+
---
+++ src/components/Header/styles.js
@@ -0,0 +1,21 @@
+import { alpha, makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  container: {
+    maxWidth: 375,
+    height: 770,
+    borderColor: '#ECECEC',
+    borderStyle: 'solid',
+    marginBottom: 25,
+  },
+  header : {
+    backgroundColor: '#20A46B',
+    color:'white',
+    fontSize:'2rem',
+    height: '3.8rem',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+
+}));
